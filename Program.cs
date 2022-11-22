bfc3d5876c2f18063a8913725e983815b0fdf21b
NatanVBraeckel <r0804023@student.thomasmore.be>
2022-11-23 00:11:40 +0100

    repo gemaakt

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Reflection.Metadata;
+using ServiceStack.Text;
+/*using JsonSerializer = System.Text.Json.JsonSerializer; NIET MEER NODIG OMDAT JSSERIALIZER OOK IN SERVICESTACK.TEXT ZIT*/
+
+namespace WebScraperYoutube
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter searchterm: ");
+            string searchterm = Console.ReadLine();
+
+            WebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            driver.Navigate().GoToUrl($"https://www.youtube.com/results?search_query={searchterm.Replace(' ', '+')}&sp=CAI%253D");
+
+            WebElement accept_terms = (WebElement)wait.Until(driver => driver.FindElement(By.XPath("//*[@id=\"content\"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]")));
+            accept_terms.Click();
+
+            List<YoutubeVideo> YoutubeVideosList = new List<YoutubeVideo>();
+
+            /*3 seconden slapen*/
+            Thread.Sleep(3000);
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var video = wait.Until(driver => driver.FindElement(By.CssSelector($"#contents > ytd-video-renderer:nth-of-type({i})")));
+
+                string link = video.FindElement(By.CssSelector("ytd-thumbnail a")).GetAttribute("href");
+                string title = video.FindElement(By.CssSelector("#video-title yt-formatted-string")).Text;
+                /*Ik snap niet waarom maar .Text werkt niet op de channel, maar innerhtml opvragen wel*/
+                string channel = video.FindElement(By.CssSelector("#metadata a")).GetAttribute("innerHTML");
+                string views = video.FindElement(By.CssSelector("#metadata-line > span:nth-child(2)")).Text;
+
+                if (views == "Geen weergaven")
+                {
+                    views = "0";
+                }
+                else
+                {
+                    views = views.Substring(0, views.IndexOf(' '));
+                }
+
+                /*Console.WriteLine($"link = {link}\ntitle = {title}\nchannel = {channel}\nviews = {views}\n");*/
+
+                YoutubeVideosList.Add(new YoutubeVideo(link, title, channel, views));
+            }
+
+            /*dit haalt de huidige dir op, zonder de bin en debugmappen. hier kan ik dan de csv en json bestanden naar zetten*/
+            string currentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+            string pathJSON = currentDirectory + @"\YoutubeVids.json";
+            string jsonString = JsonSerializer.SerializeToString(YoutubeVideosList);
+            File.WriteAllText(pathJSON, jsonString);
+
+            string pathCSV = currentDirectory + @"\YoutubeVids.csv";
+            string csvString = CsvSerializer.SerializeToString(YoutubeVideosList);
+            File.WriteAllText(pathCSV, csvString);
+
+            driver.Quit();
+        }
+    }
+}
---
+++ YoutubeVideo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraperYoutube
+{
+    public class YoutubeVideo
+    {
+
+        public YoutubeVideo(string link, string title, string channel, string views)
+        {
+            Link = link;
+            Title = title;
+            Channel = channel;
+            Views = views;
+        }
+
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Channel { get; set; }
+        public string Views { get; set; }
+    }
+}
+
